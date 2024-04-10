424e7d12594222c8966ef49db8ffcad7e72b044c
like115 <258861342@qq.com>
2024-04-10 17:13:46 +0800

    sd

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -3,17 +3,112 @@ import Person from './components/Person.vue';
 </script>
 
 <template>
-  <div class="content">
-    <Person />
+
+  <div class="common-layout">
+
+    <el-container>
+      <el-header>
+        <el-row class="row-bg" justify="end">
+          <el-col :span="6">
+            <el-button type="primary">首页</el-button>
+          </el-col>
+          <el-col :span="6">
+            <el-button type="primary">新闻</el-button>
+          </el-col>
+          <el-col :span="6">
+            <el-button type="primary">列表</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <!--  -->
+          <el-col >
+            <h5 class="mb-2">Default colors</h5>
+            <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+              <el-sub-menu index="1">
+                <template #title>
+                  <el-icon>
+                    <location />
+                  </el-icon>
+                  <span>Navigator One</span>
+                </template>
+                <el-menu-item-group title="Group One">
+                  <el-menu-item index="1-1">item one</el-menu-item>
+                  <el-menu-item index="1-2">item two</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group Two">
+                  <el-menu-item index="1-3">item three</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="1-4">
+                  <template #title>item four</template>
+                  <el-menu-item index="1-4-1">item one</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-menu-item index="2">
+                <el-icon><icon-menu /></el-icon>
+                <span>Navigator Two</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <el-icon>
+                  <document />
+                </el-icon>
+                <span>Navigator Three</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <el-icon>
+                  <setting />
+                </el-icon>
+                <span>Navigator Four</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+
+
+          <!--  -->
+        </el-aside>
+        <el-main>
+          Main
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
+
+
 
 </template>
 
 <style scoped>
-.content {
-  background-color: #ddd;
-  border-radius: 10px;
-  padding: 10px;
-  box-shadow: 0 0 10px;
+.el-col {}
+
+.el-header {
+  padding: 15px 20px;
+  /* background-color:#019fa0; */
+}
+
+.el-aside {
+
+  /* background-color: #ff5675; */
+}
+
+.el-main {
+  background-color: #ff8345;
+}
+
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
 }
 </style>
