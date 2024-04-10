94e7b82beb8326609898f3401bc465029d335785
like115 <258861342@qq.com>
2024-04-10 17:24:53 +0800

    sd

--- vue01/src/App.vue
+++ vue01/src/App.vue
@@ -25,41 +25,22 @@ import Person from './components/Person.vue';
           <!--  -->
           <el-col >
             <h5 class="mb-2">Default colors</h5>
-            <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-              <el-sub-menu index="1">
-                <template #title>
-                  <el-icon>
-                    <location />
-                  </el-icon>
-                  <span>Navigator One</span>
-                </template>
-                <el-menu-item-group title="Group One">
-                  <el-menu-item index="1-1">item one</el-menu-item>
-                  <el-menu-item index="1-2">item two</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="Group Two">
-                  <el-menu-item index="1-3">item three</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="1-4">
-                  <template #title>item four</template>
-                  <el-menu-item index="1-4-1">item one</el-menu-item>
-                </el-sub-menu>
-              </el-sub-menu>
-              <el-menu-item index="2">
+            <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+              <el-menu-item index="1">
                 <el-icon><icon-menu /></el-icon>
-                <span>Navigator Two</span>
+                <span>Navigator 1</span>
               </el-menu-item>
-              <el-menu-item index="3" disabled>
+              <el-menu-item index="2" >
                 <el-icon>
                   <document />
                 </el-icon>
-                <span>Navigator Three</span>
+                <span>Navigator 2</span>
               </el-menu-item>
-              <el-menu-item index="4">
+              <el-menu-item index="3">
                 <el-icon>
                   <setting />
                 </el-icon>
-                <span>Navigator Four</span>
+                <span>Navigator 3</span>
               </el-menu-item>
             </el-menu>
           </el-col>
