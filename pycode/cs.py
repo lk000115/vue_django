f8b11959ccabd46731858ace00793c56d0e55ddd
like115 <258861342@qq.com>
2024-04-11 16:35:20 +0800

    sd

--- pycode/cs.py
+++ pycode/cs.py
@@ -1,1 +1,11 @@
-import pandas
+import pandas as pd
+df = pd.read_excel('gz.xlsx')
+
+# from openpyxl  import load_workbook 
+
+# wb = load_workbook(filename='gz.xlsx')
+# st = wb.sheetnames[0]    # 获取表格的第一个工作表名
+# sheet = wb[st]
+
+print(df)
+
