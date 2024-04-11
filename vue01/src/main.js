56366103930784461e222a7b2cfdd12be3b35f0f
like115 <258861342@qq.com>
2024-04-11 09:45:30 +0800

    sd

--- vue01/src/main.js
+++ vue01/src/main.js
@@ -1,15 +1,18 @@
 import { createApp } from 'vue'
 // import './style.css'
+
+//导入element
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+//导入路由
 import router from './router/index'
 
+//设定启动的APP入口组件
 import App from './App.vue'
 
+
 const app = createApp(App)
-
-
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
