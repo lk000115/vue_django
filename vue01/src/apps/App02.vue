e68203ac169b274cccafc5c68ec78452a1edf8b5
like115 <258861342@qq.com>
2024-04-17 17:08:59 +0800

    sd

--- vue01/src/apps/App02.vue
+++ vue01/src/apps/App02.vue
@@ -1,32 +1,27 @@
 <template>
 
-  <div class="common-layout">
-    <Header/>
-    <el-container>
-      <el-aside width="200px">
-        Aside
-    </el-aside>
-      <el-main>
-        <router-view></router-view>
-    </el-main>
-    </el-container>
-  </div>
+    <div class="common-layout">
+        <Header />
+        <el-container>
+            <el-aside width="200px">
+                Aside
+            </el-aside>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </div>
 
 
-  
-        
-    
+
 </template>
 
-<script   setup>
+<script setup>
 import Header from '@/components/Header.vue'
-import{RouterView,RouterLink} from 'vue-router' 
+import { RouterView, RouterLink } from 'vue-router'
 
 
 </script>
 
 
-<style scoped>
-
-
-</style> 
+<style scoped></style>
