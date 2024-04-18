16a49c2c261fb773b3ca5cf7ede5d06f4fc5d705
like115 <258861342@qq.com>
2024-04-18 17:03:26 +0800

    sd

--- vue01/src/pages/02_路由组件/Detail.vue
+++ vue01/src/pages/02_路由组件/Detail.vue
@@ -1,10 +1,22 @@
 <template>
-<div >
-    详情页面
-</div>
+    <div>
+        <ul>
+            <li>编号:{{query.id}}</li>
+            <li>内容:{{query.content}}</li>
+        </ul>
+    </div>
 </template>
 <script   setup>
-
+import{toRefs} from 'vue'
+import{useRoute} from 'vue-router'
+const route = useRoute()
+console.log(route.query)
+// route是响应式对象,如果不用toRefs包裹将丢失响应式特性
+let {query} = toRefs(route)  
 </script>
+
+
+
+
 <style scoped>
 </style> 
--- vue01/src/pages/02_路由组件/News.vue
+++ vue01/src/pages/02_路由组件/News.vue
@@ -5,10 +5,9 @@
 
                 <el-menu class="el-menu-vertical-demo" :router="true">
 
-                    <el-menu-item v-for="(list, i) in newslist" :key="i" :index="list.path">
-                        <el-icon>
-                            <setting />
-                        </el-icon>
+                    <el-menu-item v-for="(list, i) in newsList" :key="i" 
+                       :route="{name:'xq',query:{id:list.id,content:list.content}}"
+                       >
                         <span>{{ list.title }}</span>
                     </el-menu-item>
 
@@ -29,9 +28,9 @@
 import { reactive } from 'vue'
 import { RouterLink, RouterView } from 'vue-router';
 const newsList = reactive([
-    { id: '001', title: '一种食物', content: '除了iPhone15之外，去年下半年发布的这些国产手机' },
-    { id: '002', title: '一种水果', content: '华为Mate60系列的强项是拍照，' },
-    { id: '003', title: '一种蔬菜', content: '国产新发布的手机激活榜，我们可以从中看出端倪来' }
+    { id: '001', title: '一种食物', content: '除了iPhone15之外，去年下半年发布的这些国产手机',path:'/news/detail/' },
+    { id: '002', title: '一种水果', content: '华为Mate60系列的强项是拍照，',path:'/news/detail/' },
+    { id: '003', title: '一种蔬菜', content: '国产新发布的手机激活榜，我们可以从中看出端倪来',path:'/news/detail/' }
 ])
 </script>
 
