64e8a059dd3e2ee21352a5781336720ff6018f51
like115 <258861342@qq.com>
2024-05-21 15:21:59 +0800

    sd

--- vue01/src/pages/03_hocks/Person.vue
+++ vue01/src/pages/03_hocks/Person.vue
@@ -2,14 +2,29 @@
 <div class="person">
     <h2>当前求和项为: {{ sum }} ,放大10倍后为{{ bigSum }}</h2>
     <button @click="add" >点我sum加1</button>
-     
+    <hr>
+    <img v-for="(dog,index) in dogList" :src="dog" :key="index">
+    <br>
+    <button @click="getDog">再来一个</button> 
 </div>
 </template>
 <script   setup>
 import useSum from './useSum'
+import useDog from './useDog'
 const {sum,add,bigSum} = useSum()
+const{dogList,getDog} = useDog()
+
 </script>
 
 <style scoped>
-
+.person{
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+img{
+    height: 100px;
+    margin-right: 10px;
+}
 </style> 
