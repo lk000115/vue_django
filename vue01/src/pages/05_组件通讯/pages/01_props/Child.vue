540e8f77694deb8f054c1e346202158bdaca192d
like115 <258861342@qq.com>
2024-05-27 16:22:25 +0800

    sd

---
+++ vue01/src/pages/05_组件通讯/pages/01_props/Child.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="child">
+    <h3>子组件</h3>
+    <h4>父给的车: {{ car }}</h4>
+    <button @click="sendToy(toy)">把玩具给父亲</button>
+</div>
+</template>
+<script   setup>
+import{ref } from 'vue'
+let toy = ref('奥特曼')
+
+defineProps(['car','sendToy'])
+
+</script>
+<style scoped>
+</style> 
---
+++ vue01/src/pages/05_组件通讯/pages/01_props/Father.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="father" >
+        <h3>父组件</h3>
+        <h4 v-show="toy">子给的玩具:{{ toy }}</h4>
+        <Child :car="car" :sendToy="getToy" />
+    </div>
+</template>
+<script   setup>
+import Child from './Child'
+import {ref}  from 'vue'
+let car = ref('奔驰')
+let toy = ref('')
+function getToy(value){
+   toy.value = value
+
+}
+
+
+</script>
+<style scoped>
+</style> 
