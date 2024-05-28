16a7390548602925ffcc3d8622570f412ca0f543
like115 <258861342@qq.com>
2024-05-28 13:59:30 +0800

    sd

--- vue01/src/pages/05_组件通讯/pages/01_props/Child.vue
+++ vue01/src/pages/05_组件通讯/pages/01_props/Child.vue
@@ -1,15 +1,16 @@
 <template>
 <div class="child">
     <h3>子组件</h3>
-    <h4>父给的车: {{ car }}</h4>
+    <h4>父给的东西: {{ car }}----{{ x }}</h4>
     <button @click="sendToy(toy)">把玩具给父亲</button>
+    <button @click="fn">调用父组件的函数</button>
 </div>
 </template>
 <script   setup>
 import{ref } from 'vue'
 let toy = ref('奥特曼')
 
-defineProps(['car','sendToy'])
+defineProps(['car','sendToy','x','fn'])
 
 </script>
 <style scoped>
--- vue01/src/pages/05_组件通讯/pages/01_props/Father.vue
+++ vue01/src/pages/05_组件通讯/pages/01_props/Father.vue
@@ -2,17 +2,21 @@
     <div class="father" >
         <h3>父组件</h3>
         <h4 v-show="toy">子给的玩具:{{ toy }}</h4>
-        <Child :car="car" :sendToy="getToy" />
+        <Child :car="car" :sendToy="getToy"  :x="a" :fn="fun" />
     </div>
 </template>
 <script   setup>
 import Child from './Child.vue'
 import {ref}  from 'vue'
+let a = ref('来自父组件的aa')
 let car = ref('奔驰')
 let toy = ref('')
 function getToy(value){
    toy.value = value
 
+}
+function fun(){
+    console.log("调用父组件的函数");
 }
 
 
