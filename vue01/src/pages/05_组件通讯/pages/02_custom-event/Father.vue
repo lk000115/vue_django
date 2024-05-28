39306cb9675683ae7200d96edc0e9836f4c512c1
like115 <258861342@qq.com>
2024-05-28 14:51:28 +0800

    sd

---
+++ vue01/src/pages/05_组件通讯/pages/02_custom-event/Father.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="father">
+    <h3>父组件</h3>
+       
+</div>
+</template>
+<script   setup>
+
+</script>
+<style scoped>
+</style> 
--- vue01/src/pages/05_组件通讯/router/index.js
+++ vue01/src/pages/05_组件通讯/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Props from '../pages/01_props/Father.vue'
-
+import Event from '../pages/02_custom-event/Father.vue'
 
 export default createRouter({
     history: createWebHistory(),
