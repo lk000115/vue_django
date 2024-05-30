40fa8e3bee561e3fee6fa15064d97b0e1e5a8d60
like115 <258861342@qq.com>
2024-05-30 17:24:48 +0800

    sd

---
+++ vue01/src/pages/05_组件通讯/pages/04_v-model/Father.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="father" >
+    <h3>父组件</h3>
+    <h4>{{ username }}</h4>
+    <h4>{{ password }}</h4>
+    <input :value = "username"  @input="username = $event.target.value"  />
+
+    <AtguiguInput   />
+</div>
+</template>
+<script   setup>
+import AtguiguInput from './AtguiguInput.vue'
+import{ref} from 'vue'
+let username = ref('zhansan')
+let password = ref('123456')
+
+
+</script>
+
+
+
+
+<style scoped>
+</style> 
--- vue01/src/pages/05_组件通讯/router/index.js
+++ vue01/src/pages/05_组件通讯/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Props from '../pages/01_props/Father.vue'
 import Event from '../pages/02_custom-event/Father.vue'
 import Bus from '../pages/03_mitt/Father.vue'
+import Model from '../pages/04_v-model/Father.vue'
 
 export default createRouter({
     history: createWebHistory(),
@@ -18,9 +19,13 @@ export default createRouter({
 			path: '/mitt',
 			component: Bus
 		},  
-   
-    ]
+        {
+			path: '/model',
+			component: Model
+		},
+		  
 
+    ]
 
 
 })
