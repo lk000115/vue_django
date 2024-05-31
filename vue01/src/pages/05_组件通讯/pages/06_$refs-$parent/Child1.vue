a978fe1cd6ff3fbc62edcfa5dce458e2d20f42d6
like115 <258861342@qq.com>
2024-05-31 17:13:55 +0800

    sd

--- vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Child1.vue
+++ vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Child1.vue
@@ -1,10 +1,14 @@
 <template>
-<div >
-    child1
+<div class="child1" >
+    <h3>子组件1</h3>
+    <h4>书籍:{{ book }}</h4>
 </div>
 </template>
 <script   setup>
+import{ref} from 'vue'
+let book =ref('look')
 
+defineExpose({book})
 </script>
 <style scoped>
 </style> 
--- vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Father.vue
+++ vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Father.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="father">
     <h3>父组件</h3>
+    <button @click="changeBook">修改子组件的数据值</button>
     <Child1 ref="c1" />
 </div>
 </template>
 <script   setup>
 import Child1 from './Child1.vue'
 import{ref} from 'vue'
-let c1 = ref("chesi")
+let c1 = ref("")
+function changeBook(){
+    c1.value.book = '修改后的值'
+
+}
+
+
+
 </script>
 <style scoped>
 </style> 
