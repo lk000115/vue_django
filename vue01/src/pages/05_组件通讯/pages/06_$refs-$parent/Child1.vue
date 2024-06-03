7c293fb3e4396380c31448a230d9ec135f151982
like115 <258861342@qq.com>
2024-06-03 16:54:06 +0800

    sd

--- vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Child1.vue
+++ vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Child1.vue
@@ -2,13 +2,19 @@
 <div class="child1" >
     <h3>子组件1</h3>
     <h4>书籍:{{ book }}</h4>
+    <button @click="minhouse($parent)">操作父组件的数据</button>
 </div>
 </template>
 <script   setup>
 import{ref} from 'vue'
-let book =ref('look')
+let book =ref('book----')
+let book2 = ref('book2')
+// 向外部提供数据
+defineExpose({book,book2})
+function minhouse(parent){
+     parent.house = '房屋变小';
+}
 
-defineExpose({book})
 </script>
 <style scoped>
 </style> 
--- vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Father.vue
+++ vue01/src/pages/05_组件通讯/pages/06_$refs-$parent/Father.vue
@@ -1,21 +1,26 @@
 <template>
 <div class="father">
     <h3>父组件</h3>
+    <h4>{{ house }}</h4>
     <button @click="changeBook">修改子组件的数据值</button>
     <Child1 ref="c1" />
+
 </div>
 </template>
 <script   setup>
 import Child1 from './Child1.vue'
 import{ref} from 'vue'
 let c1 = ref("")
+let house = ref('房屋')
 function changeBook(){
     c1.value.book = '修改后的值'
 
 }
-
+defineExpose({house})
 
 
 </script>
 <style scoped>
+
+
 </style> 
