876c4f17f03e92376ceac9b3cbc3eefb517e88ef
like115 <258861342@qq.com>
2024-07-16 11:37:06 +0800

    sd

--- pycode/练习实例/cs01.py
+++ pycode/练习实例/cs01.py
@@ -1,2 +1,27 @@
 print("fffff")
 print("fffff")
+
+# 反射 --函数
+
+class F():
+    def get(self):
+        print("get---函数")
+
+obj = F()
+obj = getattr(obj,"get","NO")   # 获取对象obj中的get函数
+
+obj()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
