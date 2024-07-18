3b30668951e8d29cf60bc95d87336362fb55a1b0
like115 <258861342@qq.com>
2024-07-18 22:41:16 +0800

    bf

--- vue01/vite.config.js
+++ vue01/vite.config.js
@@ -5,6 +5,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    open:true,
+    port:8081
+  },
   plugins: [vue()],
   resolve:{
     alias:{
