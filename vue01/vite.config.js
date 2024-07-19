56ce33adf3ab21200d0c53e26a5d9aaef83eb538
like115 <258861342@qq.com>
2024-07-19 08:48:43 +0800

    sd

--- vue01/vite.config.js
+++ vue01/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +13,16 @@ export default defineConfig({
     open:true,
     port:8081
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  
+  ],
   resolve:{
     alias:{
       '@': path.resolve(__dirname,'./src'),
