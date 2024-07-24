b4f38f7d410252d32be2956908c51bfb437de87a
like115 <258861342@qq.com>
2024-07-24 11:51:46 +0800

    sd

---
+++ vue01/src/pages/vite+vant/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter,createWebHashHistory } from "vue-router";
+
+routes = [
+
+    {},
+
+  
+
+]
+
+const router = createRouter({
+
+   history: createWebHashHistory(),
+   routes, 
+
+}) 
+
+export default router
