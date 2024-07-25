b596501fe05aa5835706370792fb49a5f9725ad3
like115 <258861342@qq.com>
2024-07-25 15:33:02 +0800

    sd

--- vue01/src/pages/vite+vant/App.vue
+++ vue01/src/pages/vite+vant/App.vue
@@ -1,12 +1,22 @@
 <template>
 <div >
     <h3>测试-----</h3>
+    <div class="box"></div>
 </div>
 </template>
 
 
 <script   setup>
 
+
+
 </script>
 <style scoped>
+.box{
+  width:200px ;
+  height: 100px;
+background-color: blue;
+
+}
+
 </style> 
