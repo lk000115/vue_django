65b5b722e696c8d11fe8294a597dc7775d597975
like115 <258861342@qq.com>
2024-07-25 17:16:25 +0800

    sd

--- vue01/src/pages/vite+vant/views/login/index.vue
+++ vue01/src/pages/vite+vant/views/login/index.vue
@@ -1,10 +1,21 @@
 <template>
-<div >
-    <van-nav-bar title="登陆" />
-</div>
+    <div>
+        <van-nav-bar title="登陆" />
+        <van-form>
+            <van-cell-group inset>
+                <van-field name="username" placeholder="用户名" />
+                <van-field type="password" name="userpwd" placeholder="密码" />
+            </van-cell-group>
+            <div style="margin: 16px;">
+                <van-button  block type="primary" native-type="submit">
+                    提交
+                </van-button>
+            </div>
+
+        </van-form>
+    </div>
 </template>
-<script   setup>
+<script setup>
 
 </script>
-<style scoped>
-</style> 
+<style scoped></style>
