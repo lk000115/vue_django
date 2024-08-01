6e27b448e6bc805a92934ddca18e00042d3c2cee
like115 <258861342@qq.com>
2024-08-01 16:51:41 +0800

    sd

--- vue01/src/pages/vite+vant/App.vue
+++ vue01/src/pages/vite+vant/App.vue
@@ -1,15 +1,25 @@
 <template>
 <div >
+    <h3>测试axios</h3>
+    <button @click="text">按钮</button>    
+
     <router-view/>
 </div>
 </template>
 
 
 <script   setup>
-
+import axios  from 'axios'
+async function text() {
+    // console.log("aaa");
+   let res = await axios.get("http://localhost:3000/user")
+   console.log(res.statusText );
+}
 
 
 </script>
+
+
 <style scoped>
 
 
--- vue01/src/pages/vite+vant/views/login/index.vue
+++ vue01/src/pages/vite+vant/views/login/index.vue
@@ -3,7 +3,7 @@
         <van-nav-bar title="登陆" class="page-nav-bar" />
         <van-form @submit="onSubmit">
             <van-cell-group inset>
-                <van-field name="username" placeholder="用户名" left-icon="manager" v-model="user.username" />
+                <van-field name="username" placeholder="用户名" left-icon="manager" v-model="user.userName" />
                 <van-field type="password" name="userPwd" placeholder="密码" left-icon="lock" v-model="user.userPwd" />
             </van-cell-group>
             <div style="margin: 16px;">
@@ -17,10 +17,31 @@
 </template>
 <script setup>
 import {reactive,toRefs} from 'vue'
+import {login} from '../../api/user.js'
+
+function useSubmit(user){
+    const onSubmit = async ()=>{
+      const res = await login(user);
+      if (res.statusText === 'ok'){
+          console.log("用户登陆成功");
+      }else{
+          console.log("用户名或密码错误");
+      }
+
+    }
+    return {
+        onSubmit,
+    }
+
+}
+
+
 const user = reactive({
-    username:'',
+    userName:'',
     userPwd:''
 })
  
+const {onSubmit} = useSubmit(user)
+
 </script>
 <style scoped></style>
