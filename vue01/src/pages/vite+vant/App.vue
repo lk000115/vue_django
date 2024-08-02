ac9fa7e34c08520f9b51b98b4779f55ee1759e0a
like115 <258861342@qq.com>
2024-08-02 14:41:13 +0800

    sd

--- vue01/src/pages/vite+vant/App.vue
+++ vue01/src/pages/vite+vant/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div >
     <h3>测试axios</h3>
-    <button @click="text">按钮</button>    
+    <button @click="text">发送请求</button>    
 
     <router-view/>
 </div>
@@ -10,9 +10,36 @@
 
 <script   setup>
 import axios  from 'axios'
+
+axios.interceptors.request.use((config)=>{
+    config.baseURL = 'http://localhost:3000'  
+    return config
+})
+
+axios.interceptors.response.use(
+    res => {
+       console.log('响应拦截器成功的回调');
+       return res  
+    },
+    err =>{
+       console.log('响应拦截器失败的回调');
+    //    return Promise.reject(err)
+       return new Promise(()=>{})  //返回一个promise空对象没相当于中断
+    }
+)
+
 async function text() {
     // console.log("aaa");
-   let res = await axios.get("http://localhost:3000/user")
+//    let res = await axios.get('/user')
+   let res = await axios({
+     url:'/user',
+     method:'POST',
+     data:{
+        "userName": "zhanjun",
+        "age": 20,
+        "userPwd": "123",
+     }
+   })
    console.log(res );
 }
 
