3d63f9623a5f744fd56afa4a3b1c4d730cf2b22d
like115 <258861342@qq.com>
2025-08-19 16:33:52 +0800

    sd

--- pycode/练习实例/cs01.py
+++ pycode/练习实例/cs01.py
@@ -13,26 +13,34 @@
 # 类名 = type("类名",(父类,),{成员1,成员2})
 
 # 以下函数就是把类的方法score变成一个变量或者说是属性,及调用时不需要()
-class Student(object):
+# class Student(object):
 
-    @property  # 装饰器
-    def score(self):
-        return self._score
-
-
-    @score.setter
-    def score(self, value):
-        if value < 0 or value > 100:
-            raise ValueError('score must between 0 ~ 100!')
-        self._score = value
-
-s = Student()
-s.score = 99    # 调用setter方法
-print(s.score)  # 调用getter方法
+#     @property  # 装饰器
+#     def score(self):
+#         return self._score
 
 
+#     @score.setter
+#     def score(self, value):
+#         if value < 0 or value > 100:
+#             raise ValueError('score must between 0 ~ 100!')
+#         self._score = value
 
+# s = Student()
+# s.score = 99    # 调用setter方法
+# print(s.score)  # 调用getter方法
 
+# 列表推导式或者生成式
+lt = [i*2 for i in range(1,11)]  # 列表推导式
+# print(lt)
+
+st = lt.__iter__()  # 获取迭代器
+
+while True:
+    try:
+        print(st.__next__())
+    except StopIteration:
+        break
 
 
 
