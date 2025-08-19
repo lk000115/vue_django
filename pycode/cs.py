3dc4c8ff50a27219549458b0b6562e73cb89a30d
like115 <258861342@qq.com>
2025-08-19 09:09:48 +0800

    sd

--- pycode/cs.py
+++ pycode/cs.py
@@ -1,5 +1,5 @@
-import pandas as pd
-df = pd.read_excel('gz.xlsx')
+# import pandas as pd
+# df = pd.read_excel('gz.xlsx')
 
 # from openpyxl  import load_workbook 
 
@@ -7,5 +7,14 @@ df = pd.read_excel('gz.xlsx')
 # st = wb.sheetnames[0]    # 获取表格的第一个工作表名
 # sheet = wb[st]
 
-print(df)
+# print(df)
+
+# 形参前面的*表示把传入的多个位置参数打包成一个元组
+def fn(*args):
+    for i in args:
+        print(i)
+
+lt = [1, 2, 3, 4, 5]
+# 实参前面的*表示把列表打散成多个位置参数
+fn(*lt)    
 
