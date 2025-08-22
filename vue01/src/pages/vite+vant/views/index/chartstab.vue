d8da6a02fdfcea2300f7ed83c39be60258b5e9c1
like115 <258861342@qq.com>
2025-08-22 11:53:14 +0800

    sd

--- vue01/src/pages/vite+vant/views/index/chartstab.vue
+++ vue01/src/pages/vite+vant/views/index/chartstab.vue
@@ -6,16 +6,7 @@
         <van-tab title="标签 3">内容 3</van-tab>
         <van-tab title="标签 4">内容 4</van-tab>
   </van-tabs>
-  <div class="box">
-     <div class="box-item">111</div>
-     <div class="box-item">222</div>
-     <div class="box-item">333</div>
-     <div class="box-item">444</div>
-     <div class="box-item">555</div>
-     <div class="box-item">666</div>
-     <div class="box-item">777</div>
-     <div class="box-item">888</div>
-  </div>
+
 
 </div>
 </template>
@@ -29,20 +20,7 @@ const active = ref(0);
 </script>
 
 <style    scoped>
-.box{
-   overflow-x: auto;
-   white-space: nowrap;
-}
-.box-item{
-  background-color: red;
-  padding:5px 20px;
-  display: inline-block;
-  margin:5px
 
-}
-.box::-webkit-scrollbar {
-    display: none; /* 隐藏滚动条 */
-}
 
 </style> 
 
